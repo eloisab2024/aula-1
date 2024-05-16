bd91ceb3fa5ea3ae20fb2aa9252921ac4ecd2958
eloisab2024 <ebrusamarello@icloud.com>
2024-05-15 21:29:02 -0300

    ok

---
+++ main.js
@@ -0,0 +1,8 @@
+const botoes = document.querySelectorAll(".botao);
+console.log(botao);
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+for(let j=0;j<botoes.length;)
+
+        botoes[i].classlist.add("ativo");
+    console.log(i);
